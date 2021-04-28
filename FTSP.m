784e46ae75e5e801e080da0dee04793eff8f7fd9
Yan Zong <yan@yzong.com>
2021-04-28 23:27:35 +0100

    Created a file to simulate the FTSP protocol

---
+++ FTSP.m
@@ -0,0 +1,6 @@
+% % ***********************************************************************
+% * Descriptoin: this file is used to simulate the performance of FTSP in 
+%               a network, and the least squares regression is used in this
+%               work
+% * Limitation: the code can run on the MATLAB R2020b
+% % ***********************************************************************
