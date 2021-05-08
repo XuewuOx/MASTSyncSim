92c77996d40a2efb2d359ab48168c18256024d8b
Daniel Dai <dxw.dai@gmail.com>
2021-05-08 18:17:03 +0100

    Clean the codes better user interfaces

--- TimeSync_consensus.m
+++ TimeSync_consensus.m
@@ -1,7 +1,7 @@
 % % ***************************************************************************
 % % * 
 % * Created on:  31 03 2021
-% * Modified on: 31.03.2021
+% * Modified on: 08.05.2021
 % * 
 % * Author:      Xuweu Dai
 % *
@@ -39,7 +39,7 @@ arbitraryNetwork=false; % false; %true;
 if arbitraryNetwork 
     % Random network generation
     nNode=20; nEdge=20;  
-%     nNode=4; nEdge=4;  
+
     [G0 Tree0]=genNet(nNode,nEdge,1);
     if isempty(G0)
        disp('Network generation and simlation cancelled');
@@ -58,21 +58,26 @@ if arbitraryNetwork
         netG=G0;
     end
     L=full(laplacian(netG));
-    
+    nEdge=numedges(netG); nNode=numnodes(netG);
 else
     % Network topology specified by the Laplacian matrix L
     typicalLaplacian
         
     % L=L12c;
-    L=Lhuan20; % 20 node circle network
-    % L=Lshu20;
-    [netG,L]=genNetbyL(L);
-    % nNode=length(L);% number of nodes
-    % nEdge=trace(L)/2; % number of edges
+    % L=Lhuan20; % 20 node circle network
+    L=Lshu21;
+    Lms=tril(L,-1)+eye(length(L)); 
+    [netG,L]=genNetbyL(Lms);
+   [netG,L]=genNetbyL(Lshu21);
+    nNode=length(L);% number of nodes
+    nEdge=trace(L)/2; % number of edges
 end
 
-% any changes to the network and clock configuraiton 
+% show the network topology
+strnet=['Network Toplogy (', num2str(nNode), ' nodes,' num2str(nEdge), ' edges)'];
+figure('name',strnet); plot(netG); title(strnet);
 fprintf("Network created with : %d nodes.%d edges", nNode,nEdge);
+
 % disp(L);
 %% Simulaiton Configuration 2a: Clock and noises Q R
 
@@ -133,9 +138,9 @@ end
 % D=[0.4563 0.0035;0.0112 0.4723];%0.1传统LMI所得K值
 % K=[0.3200 0.0450;0.2330 0.0511];%线性搜索LMI所得K值, Hu,Sec 5, 
 
-Kname='Knewa';  % give a name to the K, for saving simulation results 
+Kname='K';  % give a name to the K, for saving simulation results 
                 % e.g., Ka_c20, Ka_tree20
-NoiseSeq=5; % name for a repoeatd simulation, for saving simulaiton reults
+NoiseSeq=13; % name for a repoeatd simulation, for saving simulaiton reults
 
 % 1. Controllers gain matrices for tree20 network 
    K01=[0.1 0;0 0.1];%原来的初始值
@@ -192,52 +197,59 @@ x0_noisefree=10^(-6)*kron(ones(nNode,1),[100,20]');
 %            100; 20; 100; 20; 100; 20; 100; 20; 100; 20];
 
 % 2. Non-identical clocks
-a0=500; % offset 100us 
+a0=0; % offset 100us 
 b0=20; % skew 20ppm
-a=40; % offset inittial value variation range: +/- 40 uniform distribution
-b=20; % skew initial value variation raange: +/- 20 uniform distribution
-iniOffset=a0+a*2*(rand(1,nNode)-0.5);
-iniSkew=b0+b*2*(rand(1,nNode)-0.5);
+a=500; % offset inittial value variation range: +/- 40 uniform distribution
+b=30; % skew initial value variation raange: +/- 20 uniform distribution
+iniOffset=a0+a*2*(rand(1,nNode)-0.5); stdOffset=sqrt((2*a)^2/12); % std for uniform dist.
+iniSkew=b0+b*2*(rand(1,nNode)-0.5); stdSkew=sqrt((2*b)^2/12); %  sqrt((max-min)^2/12)std for uniform dist.
 x0=10^(-6)*reshape([iniOffset;iniSkew],[],1);
-x0=10^(-6)*[10:10:2*nNode*10];
+% x0=10^(-6)*[10:10:2*nNode*10];
+% illusrate the distributation of initial values 
+% figure('name','distribution of inital values'); 
+% subplot(2,1,1);hold on; plot(x0(1:2:end-1),'o-r'); title('Offset Initial Values'); grid on;ylabel('sec'); xlabel('node ID');
+% subplot(2,1,2);hold on; plot(x0(2:2:end),'o-r'); title('Skew Initial Values');grid on;ylabel('ppm-like');xlabel('node ID');
+
 % x0(2)=0; x0(1)=0; % set node 1 as ideal pefect clock
 % x0=10^(-6)*[99.36; 19.25; 99.3; 19.3; 98.7; 18.5; 99.8; 19.8; 99.36; 19.5; ...
 %             99.3; 19.3; 98.7; 18.5; 99.8; 19.8; 99.36;19.5; 99.3;  19.3;
 %             98.7; 18.5; 99.8; 19.8];
 fprintf("Clock initial values:\r");
-fprintf("    Offset %d +/- %d us \r", a0, a);
-fprintf("    Skew  %d +/- %d ppm\r ", b0, b);
+fprintf("    Offset %d +/- %d us (std=%d us) \r", a0, a, stdOffset);
+fprintf("    Skew  %d +/- %d ppm  (std=%d ppm)\r ", b0, b,stdSkew);
 
 % internal variables for clock state, output and sync errors
 % %noKalman
-y1=zeros(2*nNode,szsim); % output, a matrix for outputs at all sz simulation steps
-xx1=zeros(2*nNode,szsim); % state, a matrix for states at all sz simulation steps
-y2=zeros(2*nNode,szsim); % output
-xx2=zeros(2*nNode,szsim); % state
+y=zeros(2*nNode,szsim); % output, a matrix for outputs at all sz simulation steps
+xx=zeros(2*nNode,szsim); % state, a matrix for states at all sz simulation steps
 
-% yerr=zeros(2*nNode,sz); % output errors
+yerr=zeros(2*nNode,szsim); % output errors
 
-indTheta=1:2:nNode*2-1;  % row indices for theta
-indSkew=2:2:nNode*2; % row indices for skew
+ indTheta=1:2:nNode*2-1;  % row indices for theta
+ indSkew=2:2:nNode*2; % row indices for skew
 
-% xx1(:,1)=x0_noisefree(1:2*nNode);
-xx1(:,1)=x0;
-y1(:,1)=xx1(:,1)+measNoisev(:,1);
-    
-xx2(:,1)=x0(1:2*nNode);
-y2(:,1)=xx2(:,1)+measNoisev(:,1);
-xx2(:,1)=x0;
-y2(:,1)=x0;
+% for root clock (node 1) failure
+% disp("node 1 failure. Disconnected from network and ignore node 1 in statistis Ybar and std, etc");
+% indTheta=3:2:nNode*2-1;  % row indices for theta
+% indSkew=4:2:nNode*2; % row indices for skew
+
+
+% xx(:,1)=x0_noisefree(1:2*nNode);
+xx(:,1)=x0(1:2*nNode);
+y(:,1)=xx(:,1)+measNoisev(:,1);
+xx(:,1)=x0;
+y(:,1)=x0;
 
 % initial errors for interation from k=2  
-yk=y1(:,1);
-yk=y2(:,1);
+yk=y(:,1);
 Ybar(:,1)=[mean(yk(indTheta)');mean(yk(indSkew)')];
+% yerr=[]; % clear yerr in case yerr has defined in previous simulation
 yerr(:,1)=yk-kron(ones(nNode,1),Ybar(:,1));%误差向量（输出-输出均值）胡equ29  ??
    
+%% Simulaiton Configuration 2d: Revise the node's role and topology
 % update the nNode and nEdge, as they may be changed due to spannning tree
-nNode=numnodes(netG);% number of nodes
-nEdge=numedges(netG); % number of edges
+% nNode=numnodes(netG);% number of nodes
+% nEdge=numedges(netG); % number of edges
 
 % Revise the network to fit the topology needs
 % Set the non-servo clock, reference clock and initial states
@@ -279,19 +291,17 @@ while true
    [netG L]=genNetbyL(L);
 end
 
+% any changes to the network and clock configuraiton 
+fprintf("Network created with : %d nodes.%d edges\n", nNode,nEdge);
 %%  Simulating the Networked Synchronization controller 
 disp('Now start simulation ');
 hfigsim=figure('Name','Simulation Animation'); 
-        cm = colormap('Lines'); 
-    subplot(2,2,1);
-    title('offset \theta of all nodes & avg');
-    subplot(2,2,2);
-    title('skew \gamma of all nodes & avg');
-    subplot(2,2,3);
-    title('errors of \theta wrt.the average');
+    cm = colormap('Lines'); 
+    subplot(2,2,1);    title('offset \theta of all nodes & avg');
+    subplot(2,2,2);    title('skew \gamma of all nodes & avg');
+    subplot(2,2,3);    title('errors of \theta wrt.the average');
     % ax = axes; ax.ColorOrder = cm;
-    subplot(2,2,4);
-    title('errors of \gamma wrt.the average');
+    subplot(2,2,4);    title('errors of \gamma wrt.the average');
     % ax = axes; ax.ColorOrder = cm;
 
 
@@ -308,108 +318,107 @@ L1=kron(L,eye(2));
 
 if chkEigAc(A,B,K,L)==false
     % the close form of the NCS has unstable eigenvalue
-    warning("Unstable eigenvalue of the networked closed loop system \n");
+    warning("     Unstable eigenvalue of the networked closed loop system \n");
 else
-    disp("stable system (two eigenvalue ==1, others <1)");
+    disp("     Good,stable system (two eigenvalue ==1, others <1)");
 end
+strK=num2str(K);
+fprintf("     K=[%s ;\n        %s]\n", strK(1,:), strK(2,:));
+fprintf("    simulaiton is in process");
+
 % Please note, B1*L1 shoulde be the same as BK1
 % sz=500;
+
 for k = 2:szsim
 
 %   % x-based simulation, State and output updates, Hu2019, eq.26, 27
-    U=L1*y2(:,k-1); % get the output differece with neighbours
+    U=L1*y(:,k-1); % get the output differece with neighbours
     % U=L1*zeros(2*nNode,1); % free running clock, no regulation
-    xx2(:,k)=A1*xx2(:,k-1)-B1*U+procNoisew(:,k-1); % state updates
-    y2(:,k)=xx2(:,k)+measNoisev(:,k); % output updates
+    xx(:,k)=A1*xx(:,k-1)-B1*U+procNoisew(:,k-1); % state updates
+    y(:,k)=xx(:,k)+measNoisev(:,k); % output updates
 
-    % y-base sys, noise-free      
-    y1(:,k)=A1*y1(:,k-1)-BK1*y1(:,k-1);%w输出Y值 胡 eq 28
-    xx1(:,k)=y1(:,k);%状态X值
-
-%    % noisy y-base system
-%     v1=measNoisev(:,k-1);  % DXW: replace he next two lines
-%     v2=measNoisev(:,k);
-%               
-%     % Rearrange the closed system as Y being the state variable of interest
-%    % y1(:,k)=A1*y1(:,k-1)-BK1*y1(:,k-1)+procNoisew(:,k-1)+v2-A1*v1;%w输出Y值 胡 eq 28
-%     y1(:,k)=A1*y1(:,k-1)-BK1*y1(:,k-1)+procNoisew(:,k-1)+v2-A1*v1+BK1*v1;%w输出Y值 胡 eq 28
-%   
-%     xx1(:,k)=y(:,k)-v2;%状态X值
-%     % proof: how x and y are swaped
-%     % y=x+v2
-%     % xk+v2=A*(xk-1+v1)-BK*(xk-1+v1)+wk-1+v2-A*v1+BK*v1
-%     % xk=A*(xk-1)-BK*(xk-1)+wk-1 
-
-
-%     % Debug codes to check if there is any mismatch between y-based simulation and
-%     % x-based one
-%     if sum(abs((y2(:,k)-y1(:,k)))) ~= 0        
-%         warning("Different output values at k=%d", k);
-%     end
-%     if sum(abs((xx2(:,k)-xx1(:,k)))) ~= 0        
-%         warning("Different state values at k=%d", k);
-%     end
-
+%     % y-base sys, noise-free      
+%     y1(:,k)=A1*y1(:,k-1)-BK1*y1(:,k-1);%w输出Y值 胡 eq 28
+%     xx1(:,k)=y1(:,k);%状态X值
     
     % collect the synchronization error for results analysis
     % Selet which reuslts to be used: 2 for x-based simulaiton, 1 for
     % y-based
-    xx=xx2;  % x-based simulaiton results % xx=xx1; for y-based simulation 
-    y=y2;  % x-based simulaiton results % Or y=y1; for y-based simulation
-    
+
     yk=y(:,k);
     Ybar(:,k)=[mean(yk(indTheta)');mean(yk(indSkew)')];
     Ystd(:,k)=[std(yk(indTheta)');std(yk(indSkew)')];
     yerr(:,k)=yk-kron(ones(nNode,1),Ybar(:,k));%误差向量（输出-输出均值）胡equ29  ??
    
 
-   
+   % for animaltion during simulation
     if (k<10) || (k<100 && (mod(k,10)==0)) ||(k>100 && mod(k,50)==0)
            figure(hfigsim);
-    subplot(2,2,1);
-    drawTrajectory(xx(indTheta,1:k))
-    hold on;  plot(1:k,Ybar(1,1:k), '-k', 'MarkerSize', 5, 'LineWidth', 2);
-    subplot(2,2,2);
-    drawTrajectory(xx(indSkew,1:k))
-    hold on;  plot(1:k,Ybar(2,1:k), '-sk', 'MarkerSize', 5,'LineWidth', 2);
-    subplot(2,2,3);
-    hold on; cla; plot(1:k,yerr(indTheta,1:k), '-o');
-    subplot(2,2,4);
-    hold on; cla; plot(1:k,yerr(indSkew,1:k), '-o','LineWidth', 1);
-     title('errors of \gamma wrt.the average');
-	 xlabel('time (s)');
-     drawnow
-    end
-    %pause(0.1)
+           fprintf(".");
 
+           % fprintf('k=%d\n',k);
+        subplot(2,2,1);   drawTrajectory(xx(indTheta,1:k))
+        hold on;  plot(1:k,Ybar(1,1:k), '-k', 'MarkerSize', 5, 'LineWidth', 2);
+        subplot(2,2,2);   drawTrajectory(xx(indSkew,1:k))
+        hold on;  plot(1:k,Ybar(2,1:k), '-sk', 'MarkerSize', 5,'LineWidth', 2);
+        subplot(2,2,3);
+        hold on; cla; plot(1:k,yerr(indTheta,1:k), '-o');
+        subplot(2,2,4);
+        hold on; cla; plot(1:k,yerr(indSkew,1:k), '-o','LineWidth', 1);
+        title('errors of \gamma wrt.the average');
+        xlabel('time (s)');
+        drawnow
+    end
+    
 end
-disp('Simulation Ends');
+
+fprintf('\n Simulation Ends\n');
 %% plot results
-simNotes=sprintf('Clk %d, ideal ref clk',6); xpos=1;nlayer=15;
-figure('Name',['Network Graph and Offset variance wrt time' Kname ', noise' num2str(NoiseSeq) ')']); 
+simNotes=sprintf('Clk %d, ideal ref clk',1); xpos=1;nlayer=5;
+
+if exist('Kname','var')    % for plot 
+    Kname='K=xx'; NoiseSeq=0; 
+end
+strConfig=['(' Kname ', noise' num2str(NoiseSeq) ')'];
+figure('name',['Output offset and offset error' strConfig]);
+ subplot(2,2,1); plot(y(indTheta,:)','.-'); title('Offset output');
+ hold on;  plot(1:k,Ybar(1,:), '-k', 'MarkerSize', 5, 'LineWidth', 2);
+ subplot(2,2,2); plot(y(indSkew,:)','.-'); title('Skew output');
+ hold on;  plot(1:k,Ybar(2,:), '-sk', 'MarkerSize', 1,'LineWidth', 2);
+ subplot(2,2,3); plot(yerr(indTheta,1:100)', '.-'); title('Offset Errors');
+ subplot(2,2,4); plot(yerr(indSkew,1:100)', '.-');title('Skew Errors');
+  xlabel('time (s)');
+
+  
+
+figure('Name',['Net Graph & Offset/Skew variance ' strConfig]); 
 k=length(Ystd);
 subplot(1,2,1);
 if issymmetric(L)
    plot(netG); %undirected graph
+   % plot(netG,'LineWidth',2);
+   % set(gca,'ydir','reverse'); % use ydir, xdir to adjust the figure's dir
+   % set(gca,'xdir','reverse'); 
 else
    % idrected graph. transpose the Laplacian for correct visulzation of
    % link's directiorn
    d=diag(L);     % D is the degree matrix 
-    A=diag(diag(L))-L'; 
-   plot(digraph(A));
+    AdjM=diag(diag(L))-L; 
+   plot(digraph(AdjM));
 end
 text(xpos-0.2,nlayer+0.2,simNotes);
-title(sprintf('Topology: %d nodes, %d edges (ref clk = [ ])',nNode, nEdge));
+% title(sprintf('Topology: %d nodes, %d edges (ref clk = [%s])',nNode, nEdge, num2str(listnonServoClk)));
 if (length(listRefClk)==0)
    title(sprintf('Topology: %d nodes, %d edges (no refence clk)',nNode, nEdge)); 
 else
    title( "Topology: "+nNode+" nodes  "+nEdge+ "edges  "+ num2str(length(listRefClk)+ " Refence clock = [" + num2str(listRefClk))+"]");
 end 
+
 %offset的方差以及滑动平均下的方差
 subplot(1,2,2);hold on;
 plot(1:k,Ystd, 'MarkerSize', 5, 'LineWidth', 1);
 hold on;
-title(['std of offset and skew among nodes (' Kname ', n' num2str(NoiseSeq) ')']);
+title(['std of offset and skew (' Kname ', n' num2str(NoiseSeq) ')']);
 mov50Offset=movmean(Ystd(1,:),50); 
 mov50Skew=movmean(Ystd(2,:),50); 
 plot(1:k,mov50Offset,'b','LineWidth', 2);%线性搜索的 
@@ -428,6 +437,8 @@ grid on;
 % M2=movmean(Ystd(2,:),10);plot(1:k,M2,'y')%线性搜索的
 
 
+
+  
 fprintf("Simulation Done!  %d nodes, %d sync steps\n",nNode,szsim);
  disp("     " + num2str(length(listnonServoClk)+ " non-servo clock = [" + num2str(listnonServoClk))+"]");
  disp("     " + num2str(length(listRefClk)+ " Refence clock = [" + num2str(listRefClk))+"]");
@@ -437,13 +448,13 @@ fprintf("   maximum std in t=[300,800]: [offset=%d skew=%d]\n",max(Ystd(1,[300:s
 fprintf("   average std in t=[300,800]: [offset=%d, skew=%d]\n", mean(Ystd(1,[300:szsim])), mean(Ystd(2,[300:szsim])));
 
 
-xvar=var(xx2(:,[300:szsim])');
+xvar=var(xx(:,[300:szsim])');
 xthetavar=[1:nNode; xvar(1:2:2*nNode-1)];
 xskewvar=[1:nNode; xvar(2:2:2*nNode)];
 
-xvarsorted=sortrows(xthetavar',[2]); % sort in ascending order based on the elements in the 2 column.
-disp("node (in ascending theta var) ="); disp(xvarsorted(:,1)');
-disp("                    theta var ="); disp(xvarsorted(:,2)');
+% xvarsorted=sortrows(xthetavar',[2]); % sort in ascending order based on the elements in the 2 column.
+% disp("node (in ascending theta var) ="); disp(xvarsorted(:,1)');
+% disp("                    theta var ="); disp(xvarsorted(:,2)');
 
 disp("Simulaiton Results Statistics End");
 return;
@@ -451,11 +462,7 @@ return;
 figure('name','Sychronization offset error');
 hold on; cla; plot(1:k,yerr(indTheta,1:k), '-.');
 
-
-y=y2;
-xx=xx2;
-
-        
+      
         figure(3)
         subplot(2,2,1)
         for kk=1:2:2*nNode-1
