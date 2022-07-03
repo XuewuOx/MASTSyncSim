d868af26ccbdf4f9995b844349e6d3508ac9a9ee
Yan Zong <yan@yzong.com>
2022-07-03 15:27:42 +0100

    Tidied up for preparing code release

--- TimeSync_consensus.m
+++ TimeSync_consensus.m
@@ -1,21 +1,26 @@
 % % ***************************************************************************
 % * Created on:  31/03/2021
-% * Modified on: 13/05/2021
+% * Modified on: 03/07/2022
 % * 
 % * Author:      Yan Zong, Xuewu Dai
 % *
 % * Descriptoin: the code in this branch is used to compare TPSN, dynamic 
 % *              controller, moving average and PISync, the corresponding 
-% *              paper is submitted to IEEE TII for peer review.              
+% *              paper is accepted by IEEE TII.              
+% *              
+% *              In academic work, please cite the following papers
+% *              Y. Zong, S. Liu, X. Liu, S. Gao, X. Dai, Z. Gao; 'Robust 
+% *              Synchronised Data Acquisition for Biometric Authentication'
+% *              IEEE Trans. Ind. Informat., (early access)
 % *
-% * IDE:         MATLAB R2020b
+% * IDE:         MATLAB R2021b
 % % ***********************************************************************
 clear all;
 close all;
 clc;
 
-% selecting the different clock offset and skew processing methods:
-% 1 -- enable; 0 -- disable
+% select the different clock offset and skew correction methods:
+% 1 is for enable, and 0 is for disable
 PISYNC = 0; 
 DYNCTRL = 1;
 MOVAVG = 0;
@@ -23,54 +28,48 @@ TPSN = 0;
 %% Simulaiton Configuration 1: Network Topology
 disp("Clock Synchronisation Simulation");
 
-szsim=1000; % simulation time
+SimTime=1000; % simulation time
 
-NetTree=[0  0  0  0  0  0  0  0  0  0;
-         -1 1  0  0  0  0  0  0  0  0;
-         -1 0  1  0  0  0  0  0  0  0;
-         -1 0  0  1  0  0  0  0  0  0;
-         -1 0  0  0  1  0  0  0  0  0;
-         -1 0  0  0  0  1  0  0  0  0;
-         -1 0  0  0  0  0  1  0  0  0;
-         -1 0  0  0  0  0  0  1  0  0;
-         -1 0  0  0  0  0  0  0  1  0;
-         -1 0  0  0  0  0  0  0  0  1]; % original Laplacian matrix
+Net=[0  0  0  0  0  0  0  0  0  0;
+    -1  1  0  0  0  0  0  0  0  0;
+    -1  0  1  0  0  0  0  0  0  0;
+    -1  0  0  1  0  0  0  0  0  0;
+    -1  0  0  0  1  0  0  0  0  0;
+    -1  0  0  0  0  1  0  0  0  0;
+    -1  0  0  0  0  0  1  0  0  0;
+    -1  0  0  0  0  0  0  1  0  0;
+    -1  0  0  0  0  0  0  0  1  0;
+    -1  0  0  0  0  0  0  0  0  1]; % Laplacian matrix
 
 nNode = 10;
-
-fprintf("The number of tree spinning network is %d \r", nNode);
+fprintf("The number of nodes in the definied network is %d \r", nNode);
 
 % directed graph
-d=diag(NetTree);  % d is the dialog vector of matrix L 
-AA=diag(d)-NetTree;
-netG=digraph(AA,'omitselfloops');
-% figure('name', 'Network Topology'); 
-GG = plot(netG,'Layout','layered','Direction','up','LineStyle','--','NodeFontName','Times New Roman','NodeFontSize',9.5,'Interpreter','latex'); 
-% by default 'Linewidth' is 0.5, and 'MarkerSize' is 4 
+d=diag(Net);  % d is the dialog vector of Laplacian matrix 
+AdjMatrix=diag(d)-Net;
+netG=digraph(AdjMatrix,'omitselfloops');
 
-highlight(GG,[1],'NodeColor',[0.6350 0.0780 0.1840], 'MarkerSize', 5.5) 
+% plot network topology, by default 'Linewidth' is 0.5, and 'MarkerSize' is 4 
+plotedgraph = plot(netG,'Layout','layered','Direction','up','LineStyle','--','NodeFontName','Times New Roman','NodeFontSize',9.5,'Interpreter','latex'); 
+
+% hightlight node 0 (which is the master node in the network)
+highlight(plotedgraph,[1],'NodeColor',[0.6350 0.0780 0.1840], 'MarkerSize', 5.5) 
+
+% rename the node index from '1 - 10' to '0 - 9'
 NodeIndex=1:1:nNode;
-for ii=1:nNode
-    NewNodeIndexTemp = num2str(NodeIndex(ii)-1);
-    NewNodeIndex(ii) = cellstr(NewNodeIndexTemp);
+for idx=1:nNode
+    NewNodeIndexTemp = num2str(NodeIndex(idx)-1);
+    NewNodeIndex(idx) = cellstr(NewNodeIndexTemp);
 end 
-labelnode(GG,NodeIndex,NewNodeIndex)
+labelnode(plotedgraph,NodeIndex,NewNodeIndex)
 
-set(gca, 'FontSize', 11, 'FontName', 'Times New Roman')  % axis configuration
-
-% set(gca, 'xlim',[0 0.7], 'XTick',0:0.1:0.7, 'LineWidth', 1.5); % axis configuration
-% set(gca,'ylim',[-40 80], 'YTick',-40:20:80); % axis configuration
-
-% xlabel('Xx', 'Interpreter','latex', 'FontSize', 11, 'FontName', 'Times New Roman');
-% ylabel('Yy', 'Interpreter','latex', 'FontSize', 11, 'FontName', 'Times New Roman');
-% title("Network Topology");
-
+set(gca, 'FontSize', 11, 'FontName', 'Times New Roman')
 set(gcf,'color','w');
 set(gcf,'renderer','Painters');
 % print -depsc -tiff -r600 -painters fig2.eps;
 % saveas(gcf,'fig2.tif');
 
-disp(NetTree);
+disp(Net);
 %% Simulaiton Configuration 2a: Clock and Packet-exchange Delay Noises (Q)
 % According to the 3Sigma rule of Gaussian distribution 99.73% of the noise 
 % will be in the range of [mu-3*sigma, mu+3*sigma] This suggests nearly all 
@@ -87,14 +86,13 @@ disp(NetTree);
 % In the mathematical version, 3 signms rule can be expressed as the
 % following: 
 % If abs(obs-mean(obs))>3*sd(obs) ===> you've got something unusual.
-%
 
-sigma1sqr=10^-12; % variance of offset noise, from Clock A of Giorgi2011
-sigma2sqr=10^-12; % variance of skew noise, from Clock A of Giorgi2011
-sigma3sqr=(4*10^(-6))^2; % variance of measurement noise, from Zong2019c
+sigma1sqr=10^-12; % variance of offset noise, from Clock A of Giorgi2011_TIM
+sigma2sqr=10^-12; % variance of skew noise, from Clock A of Giorgi2011_TIM
+sigma3sqr=(4*10^(-6))^2; % variance of measurement noise, from Zong2020_TIE
 
-mu=[0 0];
-R=[sigma1sqr 0;0 sigma2sqr]; % covariance of process noise (NO USE)
+mu=[0 0]; % mean value
+R=[sigma1sqr 0;0 sigma2sqr]; % covariance of process noise (no use in this work)
 Q=[sigma3sqr sigma3sqr;sigma3sqr 2*sigma3sqr]; % covariance of measurement noise
 
 fprintf("noise levels:\r");
@@ -102,27 +100,27 @@ fprintf("    offset process noise std = %d\r", sqrt(sigma1sqr));
 fprintf("    skew process noise std = %d\r", sqrt(sigma2sqr));
 fprintf("    offset measurement noise std = %d\r", sqrt(sigma3sqr));
 
-% for the k-th node, the noises is a 2-by-sz matrix stated in  
-% odd row of procNoise are (1) process noses of theta  (1-by-sz) 
-% even row of procNoise are (2) process noise of gamma (1-by-sz)
-% odd row of measNoise are (1) measurement noise of theta (1-by-sz)
-% odd row of measNoise are (2) measurement noise of gamma (1-by-sz)
+% for the k-th node, the noise is a 2-by-simTime matrix stated in  
+% odd row of procNoise are (1) process noise of theta  (1-by-simTime) 
+% even row of procNoise are (2) process noise of gamma (1-by-simTime)
+% odd row of measNoise are (1) measurement noise of theta (1-by-simTime)
+% even row of measNoise are (2) measurement noise of gamma (1-by-simTime)
 
 procNoise=[]; % the system noise due to clock skew and offset
 measNoise=[]; % the measurement noise due to timestamping uncertainty
 
 for k=1:nNode
-    procNoise((k-1)*2+1:(k-1)*2+2,:)=[sqrt(sigma1sqr)*randn(1,szsim);sqrt(sigma2sqr)*randn(1,szsim)];
-    measNoise((k-1)*2+1:(k-1)*2+2,:)=mvnrnd(mu,Q,szsim)'; 
+    procNoise((k-1)*2+1:(k-1)*2+2,:)=[sqrt(sigma1sqr)*randn(1,SimTime);sqrt(sigma2sqr)*randn(1,SimTime)];
+    measNoise((k-1)*2+1:(k-1)*2+2,:)=mvnrnd(mu,Q,SimTime)'; 
 end
 
 % processing delay due to the architecture of microprocessor
-eta_mean=120/1000000; % the mean value of processing delay is 120us, according to IoTJ special issue
+eta_mean=120/1000000; % the mean value of processing delay is 120us, according to Zong2022a_IoTJ
 eta_dev=4*10^(-6); % the std deviation of processing delay is around 0.3us
-eta = zeros(2*nNode, szsim);
+eta = zeros(2*nNode, SimTime);
 
 for k=2:nNode
-    eta(2*k-1,:)=[eta_mean+eta_dev*randn(1,szsim)];
+    eta(2*k-1,:)=[eta_mean+eta_dev*randn(1,SimTime)];
 end
 
 %% Simulaiton Configuration 2b: Controller Design  
@@ -142,7 +140,7 @@ if (PISYNC == 1 | TPSN == 1)
     K = [alpha 0; 0 beta];
 
     format long   
-    fprintf("Static controller gain K is given by using LMI:\n"); disp(K);
+    fprintf("Controller gain K is given by:\n"); disp(K);
     format short
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -169,7 +167,7 @@ if (DYNCTRL == 1 | MOVAVG == 1)
     D_K = K(3:4, 3:4);    
         
     format long   
-    fprintf("Dynamic controller gain A_K, B_K, C_K, D_K are given by using LMI:\n"); 
+    fprintf("Dynamic controller gains A_K, B_K, C_K, D_K are given:\n"); 
     fprintf("A_K = \n"); disp(A_K);
     fprintf("B_K = \n"); disp(B_K);
     fprintf("C_K = \n"); disp(C_K);
@@ -184,7 +182,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Simulaiton Configuration 2c: Clock & Networked State Initialisation
-% initialising the clock offset and skew (between 0 and 50ppm)
+% initialise the clock offset and skew (between 0 and 50ppm)
 a0=600000; % initial offset is 600ms 
 b0=25; % initial skew is 25ppm
 a=200000; % initial offset variation range: +/- 200 ms uniform distribution
@@ -194,35 +192,35 @@ stdOffset=sqrt((2*a)^2/12); % std. dev. for uniform dist. sqrt((max-min)^2/12)
 gamma0=b0+b*2*(rand(1,nNode)-0.5); 
 stdSkew=sqrt((2*b)^2/12); % std. dev. for uniform dist.
 x0=10^(-6)*reshape([theta0;gamma0],[],1); % reshape matrix to have specified 
-                                              % number (i.e. 1) of columns,
-                                              % the unit is "us"
+                                          % number (i.e. 1) of columns, the 
+                                          % unit is "us"
 fprintf("initial system state x[0]:\r");
 fprintf("    clock offset %d +/- %d us, & std=%d us \r", a0, a, stdOffset);
 fprintf("    clock skew %d +/- %d ppm, & std=%d ppm \r", b0, b, stdSkew);
 
-T=1; % clock synchronization interval
+T=1; % clock synchronisation interval
 A=[1 T;0 1];
 B=[1 0;0 1];
 
 % internal variables for clock state, output and sync errors
-x=zeros(2*nNode,szsim); % state, a matrix for states at all sz simulation steps
-y=zeros(2*nNode,szsim); % output, a matrix for outputs at all sz simulation steps
-yerr=zeros(2*nNode,szsim); % synchronisation error
-Beta=zeros(nNode,szsim); % history of tunned betas
+x=zeros(2*nNode,SimTime); % state, a matrix for states at all simulation steps
+y=zeros(2*nNode,SimTime); % output, a matrix for outputs at all simulation steps
+yerr=zeros(2*nNode,SimTime); % synchronisation error
+Beta=zeros(nNode,SimTime); % history of tunned betas
 
 indTheta=1:2:nNode*2-1;  % row index for theta
 indSkew=2:2:nNode*2; % row index for skew
 
-% for first row
+% k=0, first column
 x(:,1)=x0(1:2*nNode); % system state
 y(:,1)=x(:,1)+measNoise(:,1); % system output
 yy(:,1)=x(:,1); % for performance evaluation
 
 % setting node 1 as a reference node
-x0(1:2)=0; % initial offset and gamme to zero (first column, first two rows)
+x0(1:2)=0; % initial offset and gamma to zero (first column, first two rows)
 x(:,1)=x0; % first column of x is assigned as first row of x0
 y([1:2],:)=0; % first two rows of y is assigned as 0 (clear first two rows) 
-yy(:,1)=x0; % first column of yy is assigned as first row of x0
+yy(:,1)=x0; % first column of yy is assigned as first column of x0
 procNoise([1:2],:)=0; % clear process noises (clear first two rows)
 measNoise([1:2],:)=0; % clear process noises (clear first two rows)
 
@@ -239,7 +237,7 @@ hfigsim=figure('Name','Simulation Animation');
     subplot(2,2,3);    title('errors of \theta wrt.the average');
     subplot(2,2,4);    title('errors of \gamma wrt.the average');
 
-A1=kron(eye(nNode),A); % Kronecker product(克罗内克积)
+A1=kron(eye(nNode),A); % Kronecker product
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % the control gain from Yildirim2018, PISync
@@ -258,35 +256,26 @@ end
 % dynamic controller gain obtained by using the LMI technique
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if (DYNCTRL == 1 | MOVAVG == 1)   
-    x_K=zeros(2*nNode,szsim);
+    x_K=zeros(2*nNode,SimTime);
     A_K1=kron(eye(nNode),A_K);
     B_K1=kron(eye(nNode),B_K);
     C_K1=kron(eye(nNode),B*C_K);
     D_K1=kron(eye(nNode),B*D_K);
 end 
 
-NetTreeTemp=kron(NetTree,eye(2)); % B1*L1 is the same as BK1
+NetTemp=kron(Net,eye(2)); % B1*L1 is the same as BK1
 
-% if chkEigAc(A,B,K,NetTree)==false
-%     % the close form of the NCS has unstable eigenvalue
-%     warning("     Unstable eigenvalue of the networked closed loop system \n");
-% else
-%     disp("     Good,stable system (two eigenvalue ==1, others <1)");
-% end
-strK=num2str(K);
-fprintf("     K=[%s ;\n        %s]\n", strK(1,:), strK(2,:));
 fprintf("    simulaiton is in process");
 
-for k = 2:szsim
+for k = 2:SimTime
 
-    % state and output updates, see eq.26, 27 in Hu2019
-    
-    
+    % state and output updates
+        
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          
     % dynamic controller gain obtained by using the LMI technique
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
     if (DYNCTRL == 1 | MOVAVG == 1)
-        UTmp=NetTreeTemp*y(:,k-1); % get the output differece with neighbours
+        UTmp=NetTemp*y(:,k-1); % get the output differece with neighbours
         x_K(:,k)=A_K1*x_K(:,k-1)+B_K1*UTmp; % x_F[k+1] = A_F * x_F[k] + B_F * y[k]    
         U = C_K1*x_K(:,k)+D_K1*UTmp; % u[k] = C_F * x_F[k] + D_F * y[k]
     end
@@ -294,13 +283,13 @@ for k = 2:szsim
     % the control gain from Yildirim2018, PISync
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          
     if (PISYNC == 1 | TPSN == 1)
-        UTmp=NetTreeTemp*y(:,k-1); % get the output differece with neighbours
+        UTmp=NetTemp*y(:,k-1); % get the output differece with neighbours
         U = B1*UTmp;
     end
     
-    % state and output updates, see eq.26, 27 in Hu2019      
+    % state and output updates 
     x(:,k)=A1*x(:,k-1)-U+procNoise(:,k-1); % state updates, 
-%     x(:,k)=A1*x(:,k-1)-U+procNoise(:,k-1)-eta(:,k-1); % state updates,     
+%    x(:,k)=A1*x(:,k-1)-U+procNoise(:,k-1)-eta(:,k-1); % state updates,     
                                                % eta is due to the prcocessing delay
     y(:,k)=x(:,k)+measNoise(:,k); % output updates
             
@@ -373,7 +362,7 @@ fprintf('\n Simulation Ends\n');
 % plotting the offset and skew outputs
 figure
 
-offset_us=zeros(szsim, nNode);
+offset_us=zeros(SimTime, nNode);
 for i=1:1:nNode
     offset_us(:, i) = 1000000 * yerr((i-1)*2+1,:)'; % change the unit to us
 end 
@@ -393,12 +382,12 @@ for i=1:1:nNode
     plot(offset_us(:, i));
     hold on;
 end 
-set(gca, 'xlim',[szsim-600 szsim], 'XTick',szsim-600:100:szsim); % axis configuration
+set(gca, 'xlim',[SimTime-600 SimTime], 'XTick',SimTime-600:100:SimTime); % axis configuration
 xlabel('Time (s)', 'Interpreter','latex', 'FontSize', 13, 'FontName', 'Times New Roman');
 ylabel('$\theta_i[k]$ ($\mu$s)', 'Interpreter','latex', 'FontSize', 13, 'FontName', 'Times New Roman');
 title('Clock offset at steady state', 'Interpreter','latex');
 
-skew_ppm=zeros(szsim, nNode);
+skew_ppm=zeros(SimTime, nNode);
 for i=1:1:nNode
     skew_ppm(:, i) = 1000000 * yerr((i-1)*2+2,:)'; % change the unit to ppm 
 end
@@ -418,13 +407,13 @@ for i=1:1:nNode
     plot(skew_ppm(:, i));
     hold on;
 end 
-set(gca, 'xlim',[szsim-600 szsim], 'XTick',szsim-600:100:szsim); % axis configuration
+set(gca, 'xlim',[SimTime-600 SimTime], 'XTick',SimTime-600:100:SimTime); % axis configuration
 xlabel('Time (s)', 'Interpreter','latex', 'FontSize', 13, 'FontName', 'Times New Roman');
 ylabel('$\gamma_i[k]$ (ppm)', 'Interpreter','latex', 'FontSize', 13, 'FontName', 'Times New Roman');
 title('Clock Skew at steady state', 'Interpreter','latex');
 
 %%
-% plotting the tunning beta 
+% plot the tunning beta 
 if PISYNC == 1
     figure
     for i=1:1:nNode
@@ -437,7 +426,7 @@ if PISYNC == 1
 end
 
 %%
-% saving the simulation results
+% save the simulation results
 if PISYNC == 1
     save('simulation_all_parameters_pisync');
     save('ts_precision_pisync','yerr');     
